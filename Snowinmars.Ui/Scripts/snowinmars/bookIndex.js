e9fcf681be0cd68dcc31af0c173723e2748f8bee
snowinmars <stuff4work@yandex.ru>
2017-04-29 07:12:03 +0400

    ui fixes.

--- Snowinmars.Ui/Scripts/snowinmars/bookIndex.js
+++ Snowinmars.Ui/Scripts/snowinmars/bookIndex.js
@@ -1,27 +1,4 @@
 ﻿(function () {
-	//var books = $(".book");
-	//var l = books.length;
-
-	//for (var j = 0; j < l; j++) {
-	//	var bookId = $(books[j]).data().id;
-
-	//	$.ajax({
-	//		url: "/book/GetAuthors/" + bookId,
-	//		type: "POST",
-	//		success: function (data) {
-	//			var authorModelIds = $("[data-id=" + data.BookId + "]").find(".authorModelIds");
-	//			var l = data.Authors.length;
-
-	//			for (var i = 0; i < l; i++) {
-	//				authorModelIds.append("<option value=\"" + data.Authors[i].Id + "\" selected=\"selected\">" + data.Authors[i].Shortcut + "</option>");
-	//			}
-
-	//			authorModelIds.prop("disabled", 'disabled');
-	//			authorModelIds.chosen({ no_results_text: "Oops, nothing found!" });
-	//		}
-	//	});
-	//}
-
 	var authorModelIds = $(".authorModelIds");
 	authorModelIds.prop("disabled", "disabled");
 	authorModelIds.chosen({ no_results_text: "Oops, nothing found!" });
@@ -29,7 +6,12 @@
 	$(".chosen-container").prop("style", "");
 	$(".chosen-container").addClass("col-xs-12");
 
-	$(".table").DataTable({
+	var table = $(".table");
+
+	$(".sk-folding-cube-parent").addClass("hidden");
+
+	table.removeClass("hidden");
+	table.DataTable({
 		"autoWidth": false 
 	});
 })();
