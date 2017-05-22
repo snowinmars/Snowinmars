96d40f94aede243416fade3d8877f3f8cc794ace
snowinmars <stuff4work@yandex.ru>
2017-05-23 03:08:39 +0400

    Added EmailService: a common email manager to send emails to the admin of the site

---
+++ Snowinmars.Ui/App_Start/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Snowinmars.Ui.AppStartHelpers;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Snowinmars.Ui.App_Start.EmailService), "Start")]
+
+namespace Snowinmars.Ui.App_Start
+{
+    public static class EmailService
+    {
+        private static EmailSender EmailSender { get; set; }
+
+        public static void Start()
+        {
+            EmailService.EmailSender = new EmailSender();
+        }
+
+        public static bool IsSmtpReady => EmailService.EmailSender.IsReady;
+
+        public static void TryLogin(string entropy) => EmailService.EmailSender.TryLogin(entropy);
+
+        public static bool SendToAdmin(string message)
+        {
+            if (!IsSmtpReady)
+            {
+                return false;
+            }
+
+            EmailService.EmailSender.Send(message);
+
+            return true;
+        }
+    }
+}
