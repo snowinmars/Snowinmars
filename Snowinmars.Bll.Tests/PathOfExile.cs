c2d40036c04bca75ad4e92bd7090b5473f3f6c0c
dmitry tkachenko <dmitry.tkachenko@rokolabs.com>
2017-08-07 02:53:06 +0400

    Refactored path of exile logic and tests.

--- Snowinmars.Bll.Tests/PathOfExile.cs
+++ Snowinmars.Bll.Tests/PathOfExile.cs
@@ -1,45 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NUnit.Framework;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace Snowinmars.Bll.Tests
 {
-    public class PathOfExile
-    {
-	    private PathOfExileLogic pathOfExileLogic;
+	public class PathOfExile
+	{
+		private PathOfExileLogic pathOfExileLogic;
 
-	    [SetUp]
-	    public void Setup()
-	    {
-			this.pathOfExileLogic = new PathOfExileLogic();
+		[Theory]
+		[TestCase(new[] { 15, 15, 10, 5, 5 }, 40, 2)]
+		[TestCase(new[] { 1, 1 }, 2, 1)]
+		[TestCase(new[] { 1, 1, 1 }, 4, null)]
+		[TestCase(new[] { 19, 13, 15, 9, 17, 7 }, 40, null)]
+		public void A(int[] values, int desiredValue, int? combinationsCount)
+		{
+			bool isPossible = combinationsCount.HasValue;
+
+			var combinations = this.pathOfExileLogic.PickQualityCombination(values, desiredValue).ToList();
+
+			Assert.False(!isPossible && combinations.Any(), "It's not possible to make a valid combinations, but logic returns something");
+			Assert.False(isPossible && !combinations.Any(), "It's possible to make a valid combinations, but logic returns nothing");
+
+			if (isPossible)
+			{
+				Assert.True(combinations.Count == combinationsCount, "Actual combinations count is not valid");
+
+				foreach (var combination in combinations)
+				{
+					Assert.True(combination.Sum() == desiredValue, $"Actual sum of combination {string.Join(",", combination.Select(a => a.ToString()))} is not valid");
+				}
+			}
 		}
 
-	    [Theory]
-		[TestCase(new[] { 15, 15, 10, 5, 5 }, 40, 2)]
-	    [TestCase(new[] { 1, 1 }, 2, 1)]
-		[TestCase(new[] { 1, 1, 1 }, 4, null)]
-		[TestCase(new[] { 19,13,15,9,17,7 }, 40, null)]
-		public void A(int[] values, int desiredValue, int? combinationsCount)
-	    {
-		    bool isPossible = combinationsCount.HasValue;
-
-		    var combinations = this.pathOfExileLogic.PickQualityCombination(values, desiredValue).ToList();
-
-			Assert.False(!isPossible && combinations.Any(), "1");
-		    Assert.False(isPossible && !combinations.Any(), "2");
-
-		    if (isPossible)
-		    {
-			    Assert.True(combinations.Count == combinationsCount);
-
-			    foreach (var combination in combinations)
-			    {
-				    Assert.True(combination.Sum() == desiredValue);
-			    }
-		    }
+		[SetUp]
+		public void Setup()
+		{
+			this.pathOfExileLogic = new PathOfExileLogic();
 		}
 	}
 }
--- Snowinmars.Bll/PathOfExileLogic.cs
+++ Snowinmars.Bll/PathOfExileLogic.cs
@@ -1,56 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Snowinmars.Bll
 {
 	public class PathOfExileLogic
 	{
+		private readonly IList<IList<int>> combinations;
+
+		public PathOfExileLogic()
+		{
+			this.combinations = new List<IList<int>>();
+		}
+
 		public IEnumerable<IList<int>> PickQualityCombination(IList<int> qualities, int desiredValue)
 		{
 			for (int i = 1; i <= qualities.Count; i++)
 			{
-				printCombination(qualities, qualities.Count, i);
+				this.FillCombinations(qualities, i);
 			}
 
-			return combinations.Where(list => list.Sum() == desiredValue);
+			return this.combinations.Where(list => list.Sum() == desiredValue);
 		}
 
-		// The main function that prints all combinations of size r
-		// in arr[] of size n. This function mainly uses combinationUtil()
-		static void printCombination(IList<int> arr, int n, int r)
+		/// <summary>
+		/// Write all combinations for array to private list
+		/// </summary>
+		/// <param name="arr"></param>
+		/// <param name="k">Size of a combination to be printed</param>
+		private void FillCombinations(IList<int> arr, int k)
 		{
 			// A temporary array to store all combination one by one
-			int[] data = new int[r];
+			int[] data = new int[k];
 
 			// Print all combination using temprary array 'data[]'
-			combinationUtil(arr, data, 0, n - 1, 0, r);
+			this.FillCombinationsRecursive(arr: arr,
+											data: data,
+											start: 0,
+											end: arr.Count - 1,
+											index: 0,
+											k: k);
 		}
 
-		static IList<IList<int>> combinations = new List<IList<int>>();
-
-		/* arr[]  ---> Input Array
-		   data[] ---> Temporary array to store current combination
-		   start & end ---> Staring and Ending indexes in arr[]
-		   index  ---> Current index in data[]
-		   r ---> Size of a combination to be printed */
-		static void combinationUtil(IList<int> arr, IList<int> data, int start, int end, int index, int r)
+		/// <param name="arr">Input Array</param>
+		/// <param name="data">Temporary array to store current combination</param>
+		/// <param name="start">Staring indexes in arr[]</param>
+		/// <param name="end">Ending indexes in arr[]</param>
+		/// <param name="index">Current index in data[]</param>
+		/// <param name="k">Size of a combination to be printed</param>
+		private void FillCombinationsRecursive(IList<int> arr, IList<int> data, int start, int end, int index, int k)
 		{
-			// Current combination is ready to be printed, print it
-			if (index == r)
+			// Current combination is ready to be saved
+			if (index == k)
 			{
-				PathOfExileLogic.combinations.Add(data.Select(a => a).ToList()); // clone
+				this.combinations.Add(data.Select(a => a).ToList()); // clone
 				return;
 			}
 
 			// replace index with all possible elements. The condition
-			// "end-i+1 >= r-index" makes sure that including one element
+			// "end - i + 1 >= k - index" makes sure that including one element
 			// at index will make a combination with remaining elements
 			// at remaining positions
-			for (int i = start; ((i <= end) && (end - i + 1 >= r - index)); i++)
+			for (int i = start; ((i <= end) && (end - i + 1 >= k - index)); i++)
 			{
 				data[index] = arr[i];
-				combinationUtil(arr, data, i + 1, end, index + 1, r);
+				this.FillCombinationsRecursive(arr: arr,
+												data: data,
+												start: i + 1,
+												end: end,
+												index: index + 1,
+												k: k);
 			}
 		}
 	}
